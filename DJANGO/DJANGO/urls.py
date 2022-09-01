9067a8ea25820f4abcac6e71d712f05cda3c2ae8
sebanma <105290908+sebanma@users.noreply.github.com>
2022-09-01 07:13:30 -0500

    01/09/2022 Final

--- DJANGO/DJANGO/urls.py
+++ DJANGO/DJANGO/urls.py
@@ -18,5 +18,7 @@ from django.urls import path,include
 
 urlpatterns = [
     path('',include('libros.urls')),
-    path('admin/', admin.site.urls),
+     path('autor/',include('autores.urls')),
+     path('accounts/', include('django.contrib.auth.urls')),
+     path('admin/', admin.site.urls),
 ]
